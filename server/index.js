f7f478778ebbad7cee836f3de5d15d742980b548
EldarSuljic <suljiceldar6@gmail.com>
2022-11-07 13:47:43 +0100

    connecting to the database

--- server/index.js
+++ server/index.js
@@ -9,3 +9,16 @@ app.use(bodyParser.json({ limit: "30mb", extended: true})); //ogranicenja neka t
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://EldarSuljic:vedobrat6@cluster0.p7wu6mi.mongodb.net/?retryWrites=true&w=majority';
+//napravio sam prethodno cloud database cluster mongodb
+const PORT = process.env.PORT || 5000;
+
+//za konekciju sa bazom podataka
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => app.listen(PORT, () => console.log(`Server runing on port: ${PORT}`)) ) 
+//ako je konekcija na bazu podataka uspjesna sta zelimo da se desi: da pozovemo nas app i onda na to zelimo da se uradi app.listen
+.catch((error) => console.log(error.message));
+//ako konekcija na bazu podataka nije uspjesna
+
+//mongoose.set('useFindAndModify', false);
+//ovo se pobrine da nema warninga u konzoli, al ova gore linije pravi gresku
