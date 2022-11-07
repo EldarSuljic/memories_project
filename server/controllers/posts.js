5eea067933e9e61fd22aac8419f7c646430cbb67
EldarSuljic <suljiceldar6@gmail.com>
2022-11-07 15:57:10 +0100

    addin get and post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,28 @@
+import PostMessage from '../models/postMessage.js';
+
 //all the hendlers for the routes are going to be here so it is more clear and more understadable
-export const getPosts = (req, res) => {
-    res.send('THIS WORKS!');
+
+//callback funciton which is going to be executed whenever user visits this route
+export const getPosts = async (req, res) => {
+    try{
+        const postMessages = await PostMessage.find(); //find is asynchronous action thats why we add await and async on fuction
+
+        res.status(200).json(postMessages); //return this if everything works (json will be an array of all messages that we have)
+    } catch (error){
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPost = async (req, res) => {
+    const post = req.body; //with post requests you have access to request.body
+
+    const newPost = new PostMessage(post); //creating a new post
+
+    try{
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(409).json({ message: error.message });
+    }
 }
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import { getPosts } from '../controllers/posts.js';
+import { getPosts, createPost } from '../controllers/posts.js';
 
 //seting out our router
 const router = express.Router();
 //now we can start adding our routes
 router.get('/', getPosts);
-//callback funciton which is going to be executed whenever user visits this route
+router.post('/', createPost);
+
+
 
 export default router;
