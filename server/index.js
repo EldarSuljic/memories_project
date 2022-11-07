4aade939a4886f1d61aebc13eb585a847673a974
EldarSuljic <suljiceldar6@gmail.com>
2022-11-07 13:09:07 +0100

    indexjs_setup_server

--- server/index.js
+++ server/index.js
@@ -3,3 +3,9 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+const app = express(); //ovo se radi sa svakom express app. inicijalizacija appa
+
+app.use(bodyParser.json({ limit: "30mb", extended: true})); //ogranicenja neka tipa za slike kad se salju zahtjevi
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
+app.use(cors());
+
