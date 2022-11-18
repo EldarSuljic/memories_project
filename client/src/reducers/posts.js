5ece696369a99a4fbeb859fb6d1f209dd129cea0
EldarSuljic <suljiceldar6@gmail.com>
2022-11-18 11:06:30 +0100

    Making an action constants

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,17 +1,18 @@
 //reducer is a function that accepts the state and action, then based on the action type do some logic and
 //returns either action or the state changed by the action, usually multiple if statements, thats why switch is prefered
+import { FETCH_ALL, CREATE, UPDATE, LIKE, DELETE } from '../constants/actionTypes';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (posts = [], action) => {
     switch (action.type) {
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post) => post._id !== action.payload);
-        case 'UPDATE':
-        case 'LIKE':
+        case UPDATE:
+        case LIKE:
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload]; // spread all the posts (....posts) and then we have to add a new post which is stored in the action.payload
         default:
             return posts;
